355877f28160d29c846e459ebfe445d179ff472a
luthfeew <luthfeew@gmail.com>
2022-10-26 09:04:44 +0700

    make:model -mcrfs Komunitas

---
+++ app/Http/Controllers/KomunitasController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Komunitas;
+use Illuminate\Http\Request;
+
+class KomunitasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Komunitas  $komunitas
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Komunitas $komunitas)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Komunitas  $komunitas
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Komunitas $komunitas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Komunitas  $komunitas
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Komunitas $komunitas)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Komunitas  $komunitas
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Komunitas $komunitas)
+    {
+        //
+    }
+}

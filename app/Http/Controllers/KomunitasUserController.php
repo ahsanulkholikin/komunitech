e8ec51c9c3e57f37abb29c9e565fea010750d4c5
luthfeew <luthfeew@gmail.com>
2022-10-26 09:05:47 +0700

    make:model -mcrfs KomunitasUser

---
+++ app/Http/Controllers/KomunitasUserController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KomunitasUser;
+use Illuminate\Http\Request;
+
+class KomunitasUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\KomunitasUser  $komunitasUser
+     * @return \Illuminate\Http\Response
+     */
+    public function show(KomunitasUser $komunitasUser)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\KomunitasUser  $komunitasUser
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(KomunitasUser $komunitasUser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\KomunitasUser  $komunitasUser
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, KomunitasUser $komunitasUser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\KomunitasUser  $komunitasUser
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(KomunitasUser $komunitasUser)
+    {
+        //
+    }
+}

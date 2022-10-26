947ba88ad124bcacc7dc53e730b3b45a74b33d0f
luthfeew <luthfeew@gmail.com>
2022-10-26 09:22:50 +0700

    Models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'komunitas_id',
+        'user_id',
+        'judul',
+        'slug',
+        'konten',
+    ];
 }
--- app/Models/PostComment.php
+++ app/Models/PostComment.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PostComment extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'post_id',
+        'user_id',
+        'komentar',
+        'parent_id',
+    ];
 }
